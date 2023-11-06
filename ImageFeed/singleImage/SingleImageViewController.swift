8063b0e77faee5e0fe2791b82f50469e8568161b
Paul Glazachev <glazachev9777@gmail.com>
2023-11-06 10:40:09 +0300

    feat: add fullscreen for singleImage screen

--- ImageFeed/singleImage/SingleImageViewController.swift
+++ ImageFeed/singleImage/SingleImageViewController.swift
@@ -2,12 +2,16 @@ import UIKit
 
 class SingleImageViewController: UIViewController{
     @IBOutlet var imageView: UIImageView!
- 
+    
     var image: UIImage!
-
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+    }
+    
+    @IBAction func ddTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
